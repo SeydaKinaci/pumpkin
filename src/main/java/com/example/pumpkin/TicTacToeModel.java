e92f596a903deb835569449ca7e19755e9b9da71
SeydaKinaci <seyda.kinaci@iths.se>
2024-11-19 15:53:59 +0100

    Lab3igen

---
+++ src/main/java/com/example/pumpkin/TicTacToeModel.java
@@ -0,0 +1,79 @@
+package com.example.pumpkin;
+
+public class TicTacToeModel {
+    private char[][] board;
+    private char currentPlayer;
+    private int playerScore;
+    private int computerScore;
+
+    public TicTacToeModel() {
+        board = new char[3][3];
+        resetBoard();
+        currentPlayer = 'X';
+    }
+
+    public void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+        currentPlayer = 'X';
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public boolean makeMove(int row, int col) {
+        if (board[row][col] == ' ') {
+            board[row][col] = currentPlayer;
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            return true;
+        }
+        return false;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void incrementPlayerScore() {
+        playerScore++;
+    }
+
+    public void incrementComputerScore() {
+        computerScore++;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public int getComputerScore() {
+        return computerScore;
+    }
+
+    public char checkWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+                return board[i][0];
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
+                return board[0][i];
+        }
+        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+            return board[0][0];
+        if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+            return board[0][2];
+        return ' ';
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') return false;
+            }
+        }
+        return true;
+    }
+}
